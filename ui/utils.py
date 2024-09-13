10bc5c1d0de1d8121abc2ef2a0bf689eadc1253b
zakharovz <zakharov.zz2002@gmail.com>
2024-09-13 05:07:38 +0300

    Update utils.py
    Add background settings

--- ui/utils.py
+++ ui/utils.py
@@ -13,6 +13,29 @@ def startup_page_ui() -> str:
         session_id = str(uuid.uuid4())
         cookie_manager.set('ajs_anonymous_id', session_id)
 
+    page_bg_img = """
+        <style>
+        .st-emotion-cache-13k62yr > div {
+        background: linear-gradient(#000000, #003200);
+        }
+        [data-testid="stHeader"] {
+        background: rgba(0,0,0,0);
+        }
+        .st-emotion-cache-1p2n2i4 div {
+        background: rgba(0,0,0,0);
+        color: rgb(250, 250, 250);
+        }
+        .st-bu {
+        background-color: rgb(38, 39, 48);
+        }
+        .st-b6 > div {
+        color: rgb(250, 250, 250);
+        }
+        </style>
+        """
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
     # st.logo(
     #     'x5_tech_logo.png',
     #     link='https://x5-tech.ru/',
